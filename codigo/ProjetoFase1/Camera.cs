9bac390b65060afa42d9055b0382c930e19f4787
Moisés <moises.moreira.bio@gmail.com>
2017-11-14 23:46:51 +0000

    first comit

---
+++ codigo/ProjetoFase1/Camera.cs
@@ -0,0 +1,84 @@
+﻿//Código desenvolvido pelos alunos Moisés Moreira, nº 13676 e Vasco Figueiredo, nº 13222, do curso EDJD 2º ano
+//2017-2018 Introdução à Programação 3D
+//1ª Fase do trabalho prático: Terreno e Câmera Surface Follow
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoFase1
+{
+    public class Camera
+    {
+        public Matrix viewMatrix;//Matriz final a passar ao terreno para a sua effect.view 
+        public Vector3 camPosition; //Posicao da Camera nos 3 eixos
+        Vector3 direction;
+        private float aspectRatio;
+
+        float yaw, roll, vel; // yaw e pitch vão adquirir valores de acordo com o rato, Vel e apenas para ajustar a velocidade da camera
+        Matrix mDirecao;// corresponde à direção da camera
+
+        //Normais e receptores de normais, que são guias para entender qual lado a camera está virado e assim mudar
+        //o comportmanto dos inputs de forma a ajustar a direção do das teclas com a direção da camera
+        Vector3 NormalZ = Vector3.Backward;
+        Vector3 NormalX = Vector3.Right;
+        Vector3 turnX; Vector3 turnZ;
+
+        public Camera(GraphicsDevice device)
+        {
+            aspectRatio = (float)device.Viewport.Width /
+              device.Viewport.Height; //fixo
+
+            //-------------------------//
+            viewMatrix = Matrix.Identity;
+            camPosition = new Vector3(64f, 5f, 64f);
+            direction = new Vector3(1, 0, 0);
+            vel = 0.3f;
+            //-------------------------//
+            viewMatrix = Matrix.CreateLookAt(
+            camPosition, //este varia conforme percorremos o terreno
+            camPosition + direction, //Da a orientação da camera
+            Vector3.Up);
+
+        }
+        public void UpdateMove(KeyboardState key, MouseState state, Terrain terrain) //Recebe os inputs do teclado, rato e recebe um terreno
+        {
+            //Recebe inputs do teclado e reage de acordo
+
+            if (key.IsKeyDown(Keys.NumPad8)) camPosition += vel * turnX; //Frente
+            if (key.IsKeyDown(Keys.NumPad2)) camPosition -= vel * turnX; //Trás
+            if (key.IsKeyDown(Keys.NumPad4)) camPosition += vel * turnZ; //Esquerda
+            if (key.IsKeyDown(Keys.NumPad6)) camPosition -= vel * turnZ; //Direita
+
+            //Recebe os inputs do rato e processa-os
+            yaw = MathHelper.ToRadians(-state.Position.X);// ve a posição do rato e transforma em radianos
+            roll = MathHelper.ToRadians(state.Position.Y);
+            mDirecao = Matrix.CreateFromYawPitchRoll(yaw, 0.0f, roll);//Usa os inputs do rato e criar uma matriz que vai rodar um vector para uma certa posição
+            direction = Vector3.Transform(NormalX, mDirecao); //direção final, que vamos somar a posição da camera para termos o Target
+
+            //Cálculo final para o eixo do X e Z (movimentos horizontais)
+            turnX = Vector3.Transform(NormalX, mDirecao);
+            turnZ = Vector3.Transform(NormalZ, mDirecao);
+
+
+            //A posição Y da câmera vai ser adquirida atravéz de uma função do terreno que cálcula o Y de acordo com a posição da câmera no eixo XZ. Esta função é explicada mais na class Terrain
+            camPosition.Y = terrain.CalculateInterpolation(camPosition.X, camPosition.Z,2);
+
+            //actualiza a Camera com os novos paramtros
+            UpdateView();
+        }
+
+        public void UpdateView() //Atualiza a viewMatrix
+        {
+            viewMatrix = Matrix.CreateLookAt(
+            camPosition,
+            camPosition + direction, Vector3.Up);
+
+        }
+    }
+}
---
+++ codigo/ProjetoFase1/Tank.cs
@@ -0,0 +1,211 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ProjetoFase1
+{
+    class Tank
+    {
+        Model myModel;
+
+        Matrix world;
+        Matrix mundo;
+        Matrix view;
+        Matrix projection;
+        BasicEffect effect;
+        float scale;
+
+        ModelBone turretBone;
+        ModelBone cannonBone;
+        ModelBone right_front_wheelbone, left_front_wheelbone, right_back_wheelbone, left_back_wheelbone;
+        ModelBone right_steerBone, left_steerBone;
+
+        Matrix cannonTransform;
+        Matrix turretTransform;
+        Matrix right_front_wheelTransform, left_front_wheelTransform, right_back_wheelTransform, left_back_wheelTransform;
+        Matrix right_steerTransform, left_steerTransform;
+
+        float turretAngle = 0.0f;
+        float cannonAngle = 0f;
+        float yaw = 0.00f;
+        float vel = 0.1f;
+        float wheelRotation = 0.0f;
+        float wheelSideRotation = 0.0f;
+        Vector3 posicao = new Vector3(64, 10, 64);
+        Vector3 normal = Vector3.Up;
+        Vector3 direcaoBase = Vector3.UnitX;
+        //Matrix rotacao = Matrix.Identity;
+
+        Matrix[] bonetransforms;
+
+        public Tank(GraphicsDevice device, ContentManager content)
+        {
+            myModel = content.Load<Model>("tank");
+            effect = new BasicEffect(device);
+            float aspectRatio = (float)device.Viewport.Width / device.Viewport.Height;
+            view = Matrix.CreateLookAt(new Vector3(0.5f, 2.0f, 2.0f), Vector3.Zero, Vector3.Up);
+            projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45.0f), aspectRatio, 0.001f, 1000.0f);
+            effect.LightingEnabled = true;
+            myModel.Root.Transform = Matrix.CreateTranslation(posicao);
+
+            scale = 0.003f;
+
+            turretBone = myModel.Bones["turret_geo"];
+            cannonBone = myModel.Bones["canon_geo"];
+
+            right_front_wheelbone = myModel.Bones["r_front_wheel_geo"];
+            left_front_wheelbone = myModel.Bones["l_front_wheel_geo"];
+            right_back_wheelbone = myModel.Bones["r_back_wheel_geo"];
+            left_back_wheelbone = myModel.Bones["l_back_wheel_geo"];
+
+            right_steerBone = myModel.Bones["r_steer_geo"];
+            left_steerBone = myModel.Bones["l_steer_geo"];
+
+            turretTransform = turretBone.Transform;
+            cannonTransform = cannonBone.Transform;
+
+            right_front_wheelTransform = right_front_wheelbone.Transform;
+            left_front_wheelTransform = left_front_wheelbone.Transform;
+            right_back_wheelTransform = right_back_wheelbone.Transform;
+            left_back_wheelTransform = left_back_wheelbone.Transform;
+
+            right_steerTransform = right_steerBone.Transform;
+            left_steerTransform = left_steerBone.Transform;
+
+            bonetransforms = new Matrix[myModel.Bones.Count];
+
+            effect.LightingEnabled = true; // turn on the lighting subsystem.
+                                           //effect.EnableDefaultLighting();
+
+            effect.DirectionalLight0.DiffuseColor = new Vector3(2f, 2f, 2f); // a red light
+            effect.DirectionalLight0.Direction = new Vector3(1, 0f, 0);  // coming along the x-axis
+            //effect.DirectionalLight0.SpecularColor = new Vector3(0, 150, 0); // with green highlights
+            effect.AmbientLightColor = new Vector3(1f, 1f, 1f);
+            //effect.EmissiveColor = new Vector3(150f, 150, 150);
+        }
+
+        public void Update(KeyboardState kb, Terrain terrain, Keys up, Keys down, Keys left, Keys rigth)
+        {
+            Matrix rotacao = Matrix.CreateFromYawPitchRoll(yaw, 0, 0);
+            Vector3 direcaoHorizontal = Vector3.Transform(direcaoBase, rotacao);
+
+            posicao.Y = terrain.CalculateInterpolation(posicao.X, posicao.Z, 0f);
+            normal = terrain.CalculateInterpolationNormal(posicao.X, posicao.Z);
+
+            Matrix translacao = Matrix.CreateTranslation(posicao);
+
+            rotacao = Matrix.Identity;
+
+            Vector3 tankNormal = Vector3.Normalize(normal);
+            rotacao.Up = tankNormal;
+
+            Vector3 tankRight = Vector3.Normalize(Vector3.Cross(direcaoHorizontal, tankNormal));
+            rotacao.Right = tankRight;
+
+            Vector3 tankDir = Vector3.Normalize(Vector3.Cross(tankNormal, tankRight));
+            rotacao.Forward = tankDir;
+
+
+            if (kb.IsKeyDown(up))
+            {
+                posicao -= vel * tankDir;
+                wheelRotation += 0.5f;
+            }
+            if (kb.IsKeyDown(down))
+            {
+                posicao += vel * tankDir;
+                wheelRotation -= 0.5f;
+            }
+
+            if (kb.IsKeyDown(rigth))
+            {
+                yaw -= MathHelper.ToRadians(1f);
+                wheelSideRotation -= MathHelper.ToRadians(1f);
+                wheelRotation += 0.25f;
+            }
+            if (kb.IsKeyDown(left))
+            {
+                yaw += MathHelper.ToRadians(1f);
+                wheelSideRotation += MathHelper.ToRadians(1f);
+                wheelRotation += 0.25f;
+            }
+            if (kb.IsKeyUp(left) && kb.IsKeyUp(rigth))
+            {
+                if (wheelSideRotation != 0)
+                {
+                    if (wheelSideRotation > 0)
+                        wheelSideRotation -= 0.05f;
+                    if (wheelSideRotation < 0)
+                        wheelSideRotation += 0.05f;
+                }
+                //wheelSideRotation -= wheelSideRotation;
+            }
+
+            if (kb.IsKeyDown(Keys.Left))
+            {
+                turretAngle -= MathHelper.ToRadians(1f);
+            }
+            if (kb.IsKeyDown(Keys.Right))
+            {
+                turretAngle += MathHelper.ToRadians(1f);
+            }
+
+            if (kb.IsKeyDown(Keys.Up))
+            {
+                cannonAngle -= MathHelper.ToRadians(1f);
+            }
+            if (kb.IsKeyDown(Keys.Down))
+            {
+                cannonAngle += MathHelper.ToRadians(1f);
+            }
+
+            //Controlo das rotações
+            if (wheelSideRotation > 0.8f)
+                wheelSideRotation = 0.8f;
+            if (wheelSideRotation < -0.8f)
+                wheelSideRotation = -0.8f;
+
+
+            myModel.Root.Transform = rotacao * Matrix.CreateScale(scale) * translacao;
+            turretBone.Transform = Matrix.CreateRotationY(turretAngle) * turretTransform;
+            cannonBone.Transform = Matrix.CreateRotationX(cannonAngle) * cannonTransform;
+
+            left_back_wheelbone.Transform = Matrix.CreateRotationX(wheelRotation) * left_back_wheelTransform;
+            right_back_wheelbone.Transform = Matrix.CreateRotationX(wheelRotation) * right_back_wheelTransform;
+            left_front_wheelbone.Transform = Matrix.CreateRotationX(wheelRotation) * left_front_wheelTransform;
+            right_front_wheelbone.Transform = Matrix.CreateRotationX(wheelRotation) * right_front_wheelTransform;
+
+            right_steerBone.Transform = Matrix.CreateRotationY(wheelSideRotation) * right_steerTransform;
+            left_steerBone.Transform = Matrix.CreateRotationY(wheelSideRotation) * left_steerTransform;
+
+            myModel.CopyAbsoluteBoneTransformsTo(bonetransforms);
+            //mundo = Matrix.CreateWorld(posicao, rotacao.Forward, rotacao.Up);
+
+        }
+
+
+
+        public void Draw(Matrix view2)
+        {
+            foreach (ModelMesh mesh in myModel.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.World = bonetransforms[mesh.ParentBone.Index];
+                    effect.View = view2;
+                    effect.Projection = projection;
+
+                    effect.EnableDefaultLighting();
+                }
+                mesh.Draw();
+            }
+        }
+    }
+}
